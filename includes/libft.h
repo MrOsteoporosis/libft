ca8338132c1f467d6de11b749de3cc501537bdaa
Andreas Verheijen <averheij@student.codam.nl>
2019-11-04 12:26:02 +0100

    norme

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: averheij <averheij@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/10/28 13:09:52 by averheij       #+#    #+#                */
-/*   Updated: 2019/11/04 12:16:58 by averheij      ########   odam.nl         */
+/*   Updated: 2019/11/04 12:25:55 by averheij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,8 @@ size_t			ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t			ft_strlcat(char *dst, const char *src, size_t dstsize);
 char			*ft_strchr(const char *s, int c);
 char			*ft_strrchr(const char *s, int c);
-char			*ft_strnstr(const char *haystack,\
-							const char *needle, size_t len);
+char			*ft_strnstr(const char *haystack, const char *needle,\
+							size_t len);
 int				ft_strncmp(const char *s1, const char *s2, size_t n);
 int				ft_atoi(const char *str);
 int				ft_isalpha(int c);
@@ -62,5 +62,6 @@ int				ft_lstsize(t_list *lst);
 void			ft_lstdelone(t_list *lst, void (*del)(void *));
 void			ft_lstclear(t_list **lst, void (*del)(void*));
 void			ft_lstiter(t_list *lst, void (*f)(void *));
-t_list			*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+t_list			*ft_lstmap(t_list *lst, void *(*f)(void *),\
+							void (*del)(void *));
 #endif
