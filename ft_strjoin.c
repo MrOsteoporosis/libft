63b9bed471121a4584ad647c20417164513051ab
Andreas Verheijen <averheij@student.codam.nl>
2019-11-06 12:08:13 +0100

    null term on join, non stack return on trim

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,13 +6,26 @@
 /*   By: averheij <averheij@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/10/30 10:29:29 by averheij       #+#    #+#                */
-/*   Updated: 2019/11/05 12:32:27 by averheij      ########   odam.nl         */
+/*   Updated: 2019/11/06 12:00:30 by averheij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+static char	*ft_fstrjoin(char *res, char const *s2, int l1, int l2)
+{
+	int		i;
+
+	i = 0;
+	while (i < l2)
+	{
+		res[i + l1] = s2[i];
+		i++;
+	}
+	return (res);
+}
+
+char		*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*res;
 	int		l1;
@@ -23,20 +36,15 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		return (NULL);
 	l1 = ft_strlen(s1);
 	l2 = ft_strlen(s2);
-	res = (char*)ft_calloc(sizeof(char), l1 + l2);
+	res = (char*)ft_calloc(sizeof(char), l1 + l2 + 1);
 	if (!res)
 		return (NULL);
+	res[l1 + l2] = '\0';
 	i = 0;
 	while (i < l1)
 	{
 		res[i] = s1[i];
 		i++;
 	}
-	i = 0;
-	while (i < l2)
-	{
-		res[i + l1] = s2[i];
-		i++;
-	}
-	return (res);
+	return (ft_fstrjoin(res, s2, l1, l2));
 }
