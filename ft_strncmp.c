8b8c2f9125b4015366919482e070f109f105e786
Andreas Verheijen <averheij@student.codam.nl>
2019-11-21 12:27:06 +0100

    strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: averheij <averheij@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/10/29 13:53:58 by averheij       #+#    #+#                */
-/*   Updated: 2019/11/14 11:07:34 by averheij      ########   odam.nl         */
+/*   Updated: 2019/11/21 12:22:22 by averheij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,10 @@
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	int		i;
+	unsigned int i;
 
 	i = 0;
-	while (i < (int)n && s1[i] && s2[i])
-	{
-		if (s1[i] != s2[i])
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	while (i < n && s1[i] && s2[i] && s1[i] == s2[i])
 		i++;
-	}
-	if (i < (int)n && ((s1[i] && !s2[i]) || (!s1[i] && s2[i])))
-		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-	return (0);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
